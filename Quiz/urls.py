4b0475e3f4fb5c0a32494c4d75bb1e0d837ad0c5
rvmoura <rvmoura.96@gmail.com>
2018-10-22 22:17:10 -0300

    Some PEP-8 fixes, removing tab indents.

--- Quiz/urls.py
+++ Quiz/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path("quiz/<ID>",views.showQuiz,name="showQuiz"),	
-	path("quiz/<ID>/submit",views.submitQuiz,name="submitQuiz"),	
+    path("quiz/<ID>", views.showQuiz, name="showQuiz"),
+    path("quiz/<ID>/submit", views.submitQuiz, name="submitQuiz"),
 ]
